5dd66e76000e3cd643a5709d90385ce6f24358c6
braiaguirre <brianaguirre.sp94@gmail.com>
2023-11-30 14:59:39 -0300

    working idea

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import * as xlsx from 'xlsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const readUploadFile = (e) => {
+        e.preventDefault()
+        if (e.target.files) {
+            const reader = new FileReader()
+            reader.onload = (e) => {
+                const data = e.target.result
+                const workbook = xlsx.read(data, { type: "array" })
+                const sheetName = workbook.SheetNames[0]
+                const worksheet = workbook.Sheets[sheetName]
+                const json = xlsx.utils.sheet_to_json(worksheet)
+                console.log(json)
+            };
+            reader.readAsArrayBuffer(e.target.files[0])
+        }
+    }    
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <h1>Delivery Hero Catalog Update</h1>
+            <form>
+                <label htmlFor="upload">Upload File</label>
+                <input
+                    type="file"
+                    name="upload"
+                    id="upload"
+                    onChange={readUploadFile}
+                />
+            </form>
+        </>
+    )
 }
 
 export default App
