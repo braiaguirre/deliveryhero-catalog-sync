396b95c04ad7ffd61c7b4e151d24dffcbe0bf528
braiaguirre <brianaguirre.sp94@gmail.com>
2023-11-30 16:28:18 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import * as xlsx from 'xlsx'
 import './App.css'
+import { useState } from 'react'
+import axios from 'axios'
+const VITE_DELIVERY_HERO_API_URL = import.meta.env.VITE_DELIVERY_HERO_API_URL
+const VITE_DELIVERY_HERO_TOKEN = import.meta.env.VITE_DELIVERY_HERO_TOKEN
+
 
 function App() {
+    const [ data, setData ] = useState({})
     const readUploadFile = (e) => {
         e.preventDefault()
         if (e.target.files) {
@@ -12,11 +18,55 @@ function App() {
                 const sheetName = workbook.SheetNames[0]
                 const worksheet = workbook.Sheets[sheetName]
                 const json = xlsx.utils.sheet_to_json(worksheet)
-                console.log(json)
-            };
+                setData(json)
+            }
             reader.readAsArrayBuffer(e.target.files[0])
         }
-    }    
+    }
+
+    
+
+    const handleUpdate = () => {
+        
+        axios.defaults.headers.common = {
+            'Authorization': `Bearer ${VITE_DELIVERY_HERO_TOKEN}`
+        }
+        
+        axios.put(VITE_DELIVERY_HERO_API_URL, {
+            "products": [
+                {
+                "sku": "7791293040516",
+                "active": false,
+                "price": 980,
+                "maximum_sales_quantity": 10
+                }
+            ]
+        })
+        
+        
+        // (async() => { 
+            
+        //     const response = await axios.put(VITE_DELIVERY_HERO_API_URL, example, {
+        //     headers: {
+        //         "Cache-Control": "no-cache",
+        //         "Content-Type": "application/x-www-form-urlencoded",
+        //     }
+        //     })
+        //     console.log(response)
+        // })
+        
+    }
+
+    const example = {
+        "products": [
+            {
+            "sku": "7791293040516",
+            "active": false,
+            "price": 980,
+            "maximum_sales_quantity": 10
+            }
+        ]
+    }
 
     return (
         <>
@@ -30,6 +80,11 @@ function App() {
                     onChange={readUploadFile}
                 />
             </form>
+            <button onClick={ handleUpdate }>Update</button>
+            <h2>Products</h2>
+            <ul>
+                { data.length && data.map((product) => <li key={ product.SKU }>{ `${ product.NOMBRE } - $ ${ product.PRECIO }` }</li>)}
+            </ul>
         </>
     )
 }
