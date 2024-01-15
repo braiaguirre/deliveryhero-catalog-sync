edf83c025796e3bffed8fb185903f0322774e127
braiaguirre <brianaguirre.sp94@gmail.com>
2024-01-15 17:46:31 -0300

    redux toolkit setup

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const reducerSlice = createSlice({
+    name: 'reducer',
+    initialState: {
+    },
+    reducers: {
+    },
+});
+
+export const { } = reducerSlice.actions;
+export default reducerSlice.reducer;
